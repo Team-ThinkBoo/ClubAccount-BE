1f64aeece93f632b63e21751c8c52de9d8ea3927
baekjaehyuk <bjh010321@naver.com>
2025-03-28 23:27:50 +0900

    TB-4/feat: 영수증 카테고리 열거형 타입

---
+++ src/main/java/com/ClubAccount_BE/receipt/domain/type/ReceiptCategory.java
@@ -0,0 +1,18 @@
+package com.ClubAccount_BE.receipt.domain.type;
+
+import lombok.Getter;
+
+@Getter
+public enum ReceiptCategory {
+    GROUP_DINING("회식비"),
+    SUPPLY_PURCHASE("물품 구매비"),
+    SUBSCRIPTION("정기 구독비"),
+    VENUE_RENTAL("대관비"),
+    OTHER("기타");
+
+    private final String displayName;
+
+    ReceiptCategory(String displayName) {
+        this.displayName = displayName;
+    }
+}
