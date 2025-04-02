88c382911f61b7df18cb4a03a2bb4b1dcb8757f5
JinHyeon <tiemo0708@naver.com>
2025-04-02 10:40:39 +0900

    chore: 빌드 및 배포 스크립트 수정, 어플리케이션 설정

--- src/main/java/com/ClubAccount_BE/ClubAccountBeApplication.java
+++ src/main/java/com/ClubAccount_BE/ClubAccountBeApplication.java
@@ -2,11 +2,9 @@ package com.ClubAccount_BE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class ClubAccountBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ClubAccountBeApplication.class, args);
     }
