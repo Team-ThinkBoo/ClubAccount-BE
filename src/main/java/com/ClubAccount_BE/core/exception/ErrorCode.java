c9e500564a5b4a32d6dd6a2a4246d77d1d9abea6
JinHyeon <tiemo0708@naver.com>
2025-04-09 15:36:59 +0900

    TB-12/fix: 아이디(이메일) 최대 글자수 제한 제거

--- src/main/java/com/ClubAccount_BE/core/exception/ErrorCode.java
+++ src/main/java/com/ClubAccount_BE/core/exception/ErrorCode.java
@@ -1,21 +1,26 @@
 package com.ClubAccount_BE.core.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    AUTHENTICATION_FAIL_FORBIDDEN("AUTH_403_FORBIDDEN", "접근 권한이 없습니다."),
-    AUTHENTICATION_FAIL_UNAUTHORIZED("AUTH_401_UNAUTHORIZED", "인증되지 않은 사용자입니다."),
-    INCORRECT_PASSWORD("AUTH_401_INCORRECT_PASSWORD", "비밀번호가 일치하지 않습니다."),
-    DUPLICATED_AUTHID("AUTH_400_DUPLICATED_AUTHID", "이미 존재하는 아이디입니다."),
-    UNAUTHORIZED("A001", "인증이 필요합니다.");
+    AUTHENTICATION_FAIL_FORBIDDEN("AUTH_403_FORBIDDEN", "접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    AUTHENTICATION_FAIL_UNAUTHORIZED("AUTH_401_UNAUTHORIZED", "인증되지 않은 사용자입니다.", HttpStatus.UNAUTHORIZED),
+    INCORRECT_PASSWORD("AUTH_401_INCORRECT_PASSWORD", "비밀번호가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED),
+    DUPLICATED_AUTHID("AUTH_400_DUPLICATED_AUTHID", "이미 존재하는 아이디입니다.", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZED("A001", "인증이 필요합니다.", HttpStatus.UNAUTHORIZED);
+
     private final String code;
     private final String message;
+    private final HttpStatus httpStatus;
 
-    ErrorCode(String code, String message) {
+    ErrorCode(String code, String message, HttpStatus httpStatus) {
         this.code = code;
         this.message = message;
+        this.httpStatus = httpStatus;
     }
+
     @Override
     public String toString() {
         return code;
