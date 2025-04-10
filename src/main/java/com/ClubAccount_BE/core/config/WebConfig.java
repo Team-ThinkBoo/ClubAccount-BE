807584caa4479a5f76a9f38239bd509dd4202689
JinHyeon <tiemo0708@naver.com>
2025-04-10 22:59:45 +0900

    TB-13/refactor: 중복된 allowCredentials(true) 제거 및 allowedHeaders("*") 추가

--- src/main/java/com/ClubAccount_BE/core/config/WebConfig.java
+++ src/main/java/com/ClubAccount_BE/core/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
+                .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
