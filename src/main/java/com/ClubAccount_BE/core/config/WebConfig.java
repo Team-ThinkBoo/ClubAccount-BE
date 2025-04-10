bbeaf696a33e6d0e6bc38c0523d152787e36f8f3
JinHyeon <tiemo0708@naver.com>
2025-04-10 23:08:43 +0900

    TB-13/refactor: PATCH 메서드 허용을 위한 CORS 설정 추가

--- src/main/java/com/ClubAccount_BE/core/config/WebConfig.java
+++ src/main/java/com/ClubAccount_BE/core/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
