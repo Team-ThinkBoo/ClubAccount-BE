de1eca8c051b166a6aeb4cc590cda7ec25c5203c
JinHyeon <42500530+tiemo0708@users.noreply.github.com>
2025-04-10 23:10:53 +0900

    [TB-13] CORS 설정에서 credentials 사용 시 origin 명시 (#15)

--- src/main/java/com/ClubAccount_BE/core/config/WebConfig.java
+++ src/main/java/com/ClubAccount_BE/core/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.ClubAccount_BE.core.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
@@ -16,5 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
