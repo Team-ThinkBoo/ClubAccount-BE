8e6cb66dffb887c941b027e95d81e843a065b453
JinHyeon <tiemo0708@naver.com>
2025-04-16 01:31:52 +0900

    TB-17/chore: 프론트 요청으로 CORS origin 추가

--- src/main/java/com/ClubAccount_BE/core/config/WebConfig.java
+++ src/main/java/com/ClubAccount_BE/core/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "http://172.20.10.3:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization", "Set-Cookie")
