d0d2deaa1253c91686aba86aabda88e1302675c6
JinHyeon <tiemo0708@naver.com>
2025-04-16 16:08:02 +0900

    TB-17/refactor: resetPassword 메서드 파라미터명 s → confirmPassword로 변경하여 가독성 향상

--- src/main/java/com/ClubAccount_BE/user/application/port/in/update/PasswordResetUseCase.java
+++ src/main/java/com/ClubAccount_BE/user/application/port/in/update/PasswordResetUseCase.java
@@ -1,7 +1,5 @@
 package com.ClubAccount_BE.user.application.port.in.update;
 
-import jakarta.validation.constraints.NotBlank;
-
 public interface PasswordResetUseCase {
-    void resetPassword(String email, String newPassword, @NotBlank(message = "비밀번호 확인은 필수입니다.") String s);
+    void resetPassword(String email, String newPassword, String confirmPassword);
 }
