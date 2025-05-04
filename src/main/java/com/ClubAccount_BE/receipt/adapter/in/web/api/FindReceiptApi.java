03f42fbcb5bd0d904e5ca4b2842465b143125b50
baekjaehyuk <bjh010321@naver.com>
2025-05-04 14:18:26 +0900

    TB-22/chore: 영수증 조회 API Swagger 설명 추가

--- src/main/java/com/ClubAccount_BE/receipt/adapter/in/web/api/FindReceiptApi.java
+++ src/main/java/com/ClubAccount_BE/receipt/adapter/in/web/api/FindReceiptApi.java
@@ -18,7 +18,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Tag(name = "Find Receipt", description = "영수증 조회 API")
 public interface FindReceiptApi {
 
-    @Operation(summary = "영수증 목록 조회", description = "파싱된 영수증 정보를 조회한다.")
+    @Operation(
+            summary = "영수증 목록 조회",
+            description = "시작일과 종료일을 기준으로 파싱된 영수증을 조회한다. "
+                    + "시작일과 종료일에 정보가 없을 경우 모든 영수증을 조회한다.")
     PagingResponse<ReceiptResponse> getReceiptList(
             @PathVariable(value = "link") UUID link,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
