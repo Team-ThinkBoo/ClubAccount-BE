2eaf7afb4b3cde190c9619a01a857f61bdd36ddd
baekjaehyuk <bjh010321@naver.com>
2025-05-17 02:15:51 +0900

    TB-30/chore: CORS 설정 변경

--- src/main/java/com/ClubAccount_BE/core/config/WebConfig.java
+++ src/main/java/com/ClubAccount_BE/core/config/WebConfig.java
@@ -1,12 +1,12 @@
 package com.ClubAccount_BE.core.config;
 
 import com.ClubAccount_BE.core.config.auth.LoginUserArgumentResolver;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -22,7 +22,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://172.20.10.3:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://172.20.10.3:5173",
+                        "https://club-account-fe.vercel.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization", "Set-Cookie")
