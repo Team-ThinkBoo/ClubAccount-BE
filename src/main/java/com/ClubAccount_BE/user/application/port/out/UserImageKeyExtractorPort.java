1a7fabdd4e12c75db00b31d88d961183d4687271
JinHyeon <tiemo0708@naver.com>
2025-05-28 05:39:01 +0900

    TB-31/refactor: s3 이미지키 추출 포트와 어답터 제거

--- src/main/java/com/ClubAccount_BE/user/application/port/out/UserImageKeyExtractorPort.java
+++
@@ -1,8 +0,0 @@
-package com.ClubAccount_BE.user.application.port.out;
-
-/**
- * S3 이미지 URL에서 객체 키(key)를 추출하는 포트
- */
-public interface UserImageKeyExtractorPort {
-    String extract(String imageUrl);
-}
